e9e37044a75ad77ec8e27f23b625d33872ba4430
Sujal Jain <jainsujal100@gmail.com>
2025-05-12 10:24:33 +0530

    Add files via upload

---
+++ car.js
@@ -0,0 +1,70 @@
+
+
+var suv=()=>{
+    var suv=document.getElementById("suv")
+    var hatch=document.getElementById("hatchback");
+    var sedan=document.getElementById("sedan");
+    var muv=document.getElementById("muv")
+    var lux=document.getElementById("lux")
+  suv.style="#suv";
+  hatch.style.display="none";
+  sedan.style.display="none";
+  muv.style.display="none";
+  lux.style.display="none";
+}
+
+var hatch=()=>{
+    var suv=document.getElementById("suv")
+    var hatch=document.getElementById("hatchback");
+    var sedan=document.getElementById("sedan");
+    var muv=document.getElementById("muv")
+    var lux=document.getElementById("lux")
+    suv.style.display="none";
+    sedan.style.display="none";
+    muv.style.display="none";
+    lux.style.display="none";
+      hatch.style="#suv";
+     // sedan.style="#suv";
+}
+var sedan=()=>{
+    var suv=document.getElementById("suv")
+    var hatch=document.getElementById("hatchback");
+    var sedan=document.getElementById("sedan");
+    var muv=document.getElementById("muv")
+    var lux=document.getElementById("lux")
+    suv.style.display="none";
+    hatch.style.display="none";
+    muv.style.display="none";
+    lux.style.display="none";
+    
+      sedan.style="#suv";
+}
+var muv=()=>{
+    var suv=document.getElementById("suv")
+    var hatch=document.getElementById("hatchback");
+    var sedan=document.getElementById("sedan");
+    var muv=document.getElementById("muv")
+    var lux=document.getElementById("lux")
+     suv.style.display="none";
+     hatch.style.display="none";
+     sedan.style.display="none";
+     lux.style.display="none";
+     
+      muv.style="#suv";
+    
+}
+var lux=()=>{
+    var suv=document.getElementById("suv")
+    var hatch=document.getElementById("hatchback");
+    var sedan=document.getElementById("sedan");
+    var muv=document.getElementById("muv")
+    var lux=document.getElementById("lux")
+    suv.style.display="none";
+    hatch.style.display="none";
+    sedan.style.display="none";
+    muv.style.display="none";
+    lux.style="#suv";
+    
+
+   
+}
